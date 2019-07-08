9896a7c9a53c8ca2374a17e7e5604834279d8ef2
Stephen Vu <44630913+stephenvuzxc@users.noreply.github.com>
2019-07-08 21:27:36 +0700

    Update bash_train.sh

--- bash_train.sh
+++ bash_train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES='0' python train_2ddense.py -b 4  -data /content/drive/My\ Drive/LITS\ Final\ Project/
+CUDA_VISIBLE_DEVICES='0' python train_2ddense.py -b 1  -data /content/drive/My\ Drive/LITS\ Final\ Project/
